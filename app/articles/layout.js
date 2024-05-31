ff0ec6c7d6bf7c7b3b74fcecf219c4ae0b36dc65
Abdo <alsaidalsaid481@gmail.com>
2024-05-31 04:52:20 +0300

    vsc

--- app/articles/layout.js
+++ app/articles/layout.js
@@ -1,19 +1,22 @@
 export const metadata = {
-    title: 'Articles Page'
-}
+	title: "articles page",
+};
 
-export default function ({children}) {
-    return(
-        <div>
-            <h1>Articles</h1>
-            <div style={{
-                marginTop: "50px",
-                background: "blue",
-                padding: "20px",
-                borderRadius: "10px"
-            }}>
-                {children}
-            </div>
-        </div>
-    )
+export default function ({ children }) {
+	return (
+		<div>
+			<h1>Articles</h1>
+
+			<div
+				style={{
+					marginTop: "50px",
+					background: "blue",
+					padding: "20px",
+					borderRadius: "10px",
+				}}
+			>
+				{children}
+			</div>
+		</div>
+	);
 }
